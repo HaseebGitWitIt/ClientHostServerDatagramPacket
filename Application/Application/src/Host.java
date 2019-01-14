c7d16c331b360a80a1c1317ccb6036881e05f829
Haseeb <haseeb01001@gmail.com>
2019-01-14 00:14:13 -0500

    Example Working

---
+++ Application/Application/src/Host.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.*;
+
+public class Host {
+
+   DatagramPacket sendPacket, receivePacket;
+   DatagramSocket sendReceiveSocket;
+
+   public Host()
+   {
+     
+   }
+
+   public void sendAndReceive()
+   {
+     
+   }
+
+   public static void main(String args[])
+   {
+     Host host = new Host();
+     host.sendAndReceive();
+   }
+}
